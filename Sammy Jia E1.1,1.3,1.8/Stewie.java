7b6f524154dbfbd3163527d1d82f0229272c868b
16jias38 <16jias38@millshighsch.org>
2015-08-19 17:38:50 -0700

    1

---
+++ Sammy Jia E1.1,1.3,1.8/Stewie.java
@@ -0,0 +1,16 @@
+
+/**
+ * E1.1 for the homework.
+ * 
+ * Sammy Jia
+ * Version 1.0 8/19/15
+ */
+public class Stewie
+{
+  public static void main(String[] args)
+  {
+      System.out.println("//////////////////////");
+      System.out.println("|| Victory is mine! ||");
+      System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
---
+++ Sammy Jia E1.1,1.3,1.8/Stewie2.java
@@ -0,0 +1,24 @@
+
+/**
+ * E1.8 of homework. Start the program with the main method.
+ * 
+ * Sammy Jia 
+ * Version 1.0 8/19/15
+ */
+public class Stewie2
+{
+ public static void main(String[] args)
+ {
+     System.out.println("//////////////////////");
+     drawStewie();
+     drawStewie();
+     drawStewie();
+     drawStewie();
+     drawStewie();
+    }
+    public static void drawStewie()
+    {
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
