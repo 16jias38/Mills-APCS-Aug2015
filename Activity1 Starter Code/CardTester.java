c76df275eb79bec4b383d637c3323fab57858612
16jias38 <16jias38@millshighsch.org>
2016-03-17 08:50:41 -0700

    k

--- Activity1 Starter Code/CardTester.java
+++ Activity1 Starter Code/CardTester.java
@@ -11,9 +11,11 @@ public class CardTester {
 		Card a=new Card("king", "diamonds", 10);
 		Card b=new Card("ace", "spades", 20);
 		Card c=new Card("four", "hearts", 1);
+		Card b2=new Card("ace", "spades", 20);
 		System.out.println(a.suit()+" "+a.rank()+" "+a.pointValue());
 		System.out.println(b.suit()+" "+b.rank()+" "+b.pointValue());
 		System.out.println(c.suit()+" "+c.rank()+" "+c.pointValue());
-		System.out.println(
+		System.out.println(b.matches(c));
+		System.out.println(b.matches(b2));
 	}
 }
